a80b3f5a6e62e2927e640791055376b141eb4205
Raúl Guadrrama Recio <raul.guadarrama@asesormasmovil.es>
2021-07-06 10:47:12 +0200

    segregate App into a new file

---
+++ lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/jobs/jobs_bloc.dart';
+import 'blocs/navigation_drawer/navigation_drawer_bloc.dart';
+import 'services/jobs_repository.dart';
+import 'views/screens/main/main_screen.dart';
+import 'views/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key, required this.jobRepository}) : super(key: key);
+
+  final JobRepository jobRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (BuildContext _) => new NavigationDrawerBloc()),
+        BlocProvider(
+          create: (_) => JobsBloc(
+            jobRepository: jobRepository,
+          )..add(JobsStarted()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'TragWork',
+        theme: coreThemeData,
+        home: MainScreen(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-import 'blocs/jobs/jobs_bloc.dart';
-import 'blocs/navigation_drawer/navigation_drawer_bloc.dart';
+import 'app.dart';
 import 'services/jobs_repository.dart';
-import 'views/screens/main/main_screen.dart';
-import 'views/theme/theme.dart';
 
 void main() async {
   await initializeDateFormatting("en_US", null);
 
   runApp(App(jobRepository: JobRepository()));
-}
-
-class App extends StatelessWidget {
-  const App({Key? key, required this.jobRepository}) : super(key: key);
-
-  final JobRepository jobRepository;
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (BuildContext _) => new NavigationDrawerBloc()),
-        BlocProvider(
-          create: (_) => JobsBloc(
-            jobRepository: jobRepository,
-          )..add(JobsStarted()),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'TragWork',
-        theme: coreThemeData,
-        home: MainScreen(),
-      ),
-    );
-  }
 }
